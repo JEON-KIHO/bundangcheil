89c4b4cdb1bcbf3af407fd8a69cab5f61a9cebbd
4cgate <rlghakswo1@naver.com>
2021-06-10 18:16:07 +0900

    test commit v3

--- api/app.js
+++ api/app.js
@@ -10,8 +10,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.use('/v2/api', require('./routes/index'));
 app.use('/v2/api/users', require('./routes/users'));
